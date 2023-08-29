3e7948b3f06a4670ab73906a468bcaf8f1435efd
Elena Kononchuk <eva.kononchuk@gmail.com>
2023-08-29 23:04:31 +0300

    added bootstrap and weather.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-          <form>
-          <div className='row'>
-            <div className='col-md-9'><input type="search" placeholder='Enter the city...'/></div>
-            <div className='col-md-3'><input type="submit" value="Search" className='btn btn-primary'/></div>
-            </div>
-            </form>
-        
-        
-        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+         This project was coded by Olena Kononchuk and is <a href="https://github.com/evakononchuk/react_search_engine" target="_blank" rel="noreferrer">open-sourced</a> on <a href="https://thunderous-klepon-328691.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
+        </footer>
       </div>
-      <footer>
-      This project was coded by Olena Kononchuk and is <a href="https://github.com/evakononchuk/react_search_engine" target="_blank" rel="noreferrer">open-sourced</a> on GitHubandhosted on <a href="https://thunderous-klepon-328691.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
-      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import axios from 'axios';
+import './App.css';
+
+export default function Weather () {
+    const [ready, setReady] = useState(false);
+    const [temperature, setTemperature] = useState(null);
+    function handleResponse(response) {
+        console.log(response.data);
+        setTemperature(Math.round(response.data.main.temp));
+        setReady(true);
+    }
+
+    if (ready) {
+        return (
+            <div className='weather'>
+              <form>
+              <div className='row'>
+                <div className='col-9 search'><input type="search" placeholder='Enter the city...' className='form-control'/></div>
+                <div className='col-3'><input type="submit" value="Search" className='btn btn-primary'/></div>
+                </div>
+                </form>
+            
+              <div className='description'>
+                <h1>{city}</h1>
+                <ul>
+                  <li>Wednesday 07:00</li>
+                  <li>Mostly sunny</li>
+                </ul>
+              </div>
+    
+              <div className='row'>
+                <div className='col'>
+                  <img src='https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png' alt="moustly sunny"/>
+                  <span className='temperature'>{temperature}</span>°C
+                </div>
+                <div className='col'>
+                  <ul>
+                    <li>Precipitation: 15%</li>
+                    <li>Humidity: 72%</li>
+                    <li>Wind: 2 km/h</li>
+                  </ul>
+                </div>
+              </div>
+            
+          </div>
+        )
+    } else {
+        const apiKey = "1a6432c5ca7b6f9b0bee45c98d54ea71";
+        let city = "Barcelona";
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    
+    
+}
